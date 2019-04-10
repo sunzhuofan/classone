91e325954c1661d1edbfebb386b7051871e7942d
sunzhuofan <739588652!qq.com>
2019-04-10 20:07:24 +0800

    third commit

--- app/src/main/java/com/def/classone/BasketballActivity.java
+++ app/src/main/java/com/def/classone/BasketballActivity.java
@@ -1,14 +1,11 @@
 package com.def.classone;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-public class basketball extends AppCompatActivity {
+public class BasketballActivity extends AppCompatActivity {
     TextView score;
     TextView score2;
 
--- app/src/main/java/com/def/classone/RateActivity.java
+++ app/src/main/java/com/def/classone/RateActivity.java
@@ -1,15 +1,20 @@
 package com.def.classone;
 
 import android.content.Intent;
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class RateActivity extends AppCompatActivity {
+
+    private final String TAG = "Rate";
+    private float dollarRate = 0.1f;
+    private float euroRate = 0.2f;
+    private float wonRate = 0.3f;
 
     EditText rmb;
     TextView show;
@@ -19,7 +24,6 @@ public class RateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rate);
-
         rmb = (EditText)findViewById(R.id.rmb);
         show = (TextView)findViewById(R.id.showMoney);
 
@@ -27,6 +31,7 @@ public class RateActivity extends AppCompatActivity {
 
     public void onClick(View btn){
         //获取用户输入内容
+        Log.i(TAG,"onClick:");
         String str = rmb.getText().toString();
         float r = 0;
         if(str.length()>0){
@@ -35,27 +40,46 @@ public class RateActivity extends AppCompatActivity {
             //提示用户输入内容
             Toast.makeText(this, "请输入内容", Toast.LENGTH_SHORT).show();
         }
-        float val = 0;
+
+        Log.i(TAG,"onClick:r="+r);
+
         if(btn.getId()==R.id.btn_dollar){
-            val = r*(1/6.7f);
-            String value = String.format("%1.2f",val);
-            show.setText(value);
+            show.setText(String.format("%.2f",r*dollarRate));
         }else if(btn.getId()==R.id.btn_euro){
-            val = r*(1/11f);
-            String value = String.format("%1.2f",val);
-            show.setText(value);
+            show.setText(String.format("%.2f",r*euroRate));
         }else{
-            val = r*500;
-            String value = String.format("%1.2f",val);
-            show.setText(value);
+            show.setText(String.format("%.2f",r*wonRate));
         }
     }
 
     public void openOne(View btn){
         //打开一个页面Activity
-        Intent hello = new Intent(this,basketball.class);
-        Intent web = new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.jd.com"));
-        startActivity(web);
+        Intent config = new Intent(this,ConfigActivity.class);
+        config.putExtra("dollar_rate_key",dollarRate);
+        config.putExtra("euro_rate_key",euroRate);
+        config.putExtra("won_rate_key",wonRate);
 
+
+        Log.i(TAG,"openOne:dollarRate="+dollarRate);
+        Log.i(TAG,"openOne:euroRate="+euroRate);
+        Log.i(TAG,"openOne:wonRate="+wonRate);
+        //调试用
+
+        //startActivity(config);
+        startActivityForResult(config,1);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode==1 && resultCode==2){
+            Bundle bundle = data.getExtras();
+            dollarRate = bundle.getFloat("key_dollar",0.1f);
+            euroRate = bundle.getFloat("key_euro",0.1f);
+            wonRate = bundle.getFloat("key_won",0.1f);
+            Log.i(TAG,"onActivityResult:dollarRate="+dollarRate);
+            Log.i(TAG,"onActivityResult:euroRate="+euroRate);
+            Log.i(TAG,"onActivityResult:wonRate="+wonRate);
+        }
     }
 }
