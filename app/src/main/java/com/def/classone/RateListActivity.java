a72dfdcffcc4b6477dcd019abe98becea492e8ea
sunzhuofan <739588652!qq.com>
2019-05-22 21:04:25 +0800

    sixth commit

--- app/src/main/java/com/def/classone/RateListActivity.java
+++ app/src/main/java/com/def/classone/RateListActivity.java
@@ -1,6 +1,8 @@
 package com.def.classone;
 
 import android.app.ListActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -14,7 +16,9 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import static android.content.ContentValues.TAG;
@@ -22,12 +26,19 @@ import static android.content.ContentValues.TAG;
 public class RateListActivity extends ListActivity implements Runnable{
     String data[] = {"wait..."};
     Handler handler;
+    private String logDate = "";
+    private final String DATE_SP_KEY = "lastRateDateStr";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_rate_list);
+
+        SharedPreferences sp = getSharedPreferences("myrate", Context.MODE_PRIVATE);
+        logDate = sp.getString(DATE_SP_KEY,"");
+        Log.i("list", "lastRateDateStr = "+logDate);
+
         List<String> list1 = new ArrayList<String>();
         for(int i=1;i<100;i++){
             list1.add("item"+i);
@@ -56,45 +67,75 @@ public class RateListActivity extends ListActivity implements Runnable{
     public void run() {
         //获取网络数据，放入list带回到主线程中
         List<String> retlist = new ArrayList<String>();
-        Document doc = null;
-        try {
-            Thread.sleep(3000);
-            doc = Jsoup.connect("http://www.boc.cn/sourcedb/whpj/").get();
-            Log.i(TAG, "run: "+doc.title());
-            Elements tables = doc.getElementsByTag("table");
+        String curDateStr = (new SimpleDateFormat("yyyy-MM-dd")).format(new Date());
+        Log.i("run","curDateStr:"+ curDateStr + " logDate=" + logDate);
+
+        if(curDateStr.equals(logDate)) {
+            //如果相等，则不从网络中获取数据
+            Log.i("run", "日期相等，从数据库中获取数据");
+            RateManager manager = new RateManager(this);
+            for(RateItem item : manager.listAll()){
+                retlist.add(item.getCurName() + "-->"+ item.getCurRate());
+            }
+
+        }else{
+            Document doc = null;
+            try {
+                //从网络获取数据
+                Log.i("run", "日期不相等，从网络中获取在线数据");
+                Thread.sleep(3000);
+                doc = Jsoup.connect("http://www.boc.cn/sourcedb/whpj/").get();
+                Log.i(TAG, "run: "+doc.title());
+                Elements tables = doc.getElementsByTag("table");
             /*int i=1;
             for(Element table : tables){
                 Log.i(TAG,"run: table["+i+"]="+ table);
                 i++;
             }*/
 
-            Element table1 = tables.get(1);
-            //Log.i(TAG,"run:table1="+table1);
-            //获取TD中的数据
-            Elements tds = table1.getElementsByTag("td");
-            for(int i=0;i<tds.size();i+=8){
-                Element td1 = tds.get(i);
-                Element td2 = tds.get(i+5);
+                Element table1 = tables.get(1);
+                //Log.i(TAG,"run:table1="+table1);
+                //获取TD中的数据
+                Elements tds = table1.getElementsByTag("td");
+                List<RateItem> rateList = new ArrayList<RateItem>();
 
-                String str1 = td1.text();
-                String val = td2.text();
+                for(int i=0;i<tds.size();i+=8){
+                    Element td1 = tds.get(i);
+                    Element td2 = tds.get(i+5);
 
-                Log.i(TAG, "run: text="+str1+"==>"+val);
-                retlist.add(str1 +"==>"+val);
+                    String str1 = td1.text();
+                    String val = td2.text();
+
+                    Log.i(TAG, "run: text="+str1+"==>"+val);
+                    retlist.add(str1 +"==>"+val);
+                    rateList.add(new RateItem(str1,val));
+                }
+
+                //把数据写入到数据库中
+                RateManager manager = new RateManager(this);
+                manager.deleteAll();
+                manager.addAll(rateList);
+
+                //记录更新日期
+                SharedPreferences sp = getSharedPreferences("myrate", Context.MODE_PRIVATE);
+                SharedPreferences.Editor edit = sp.edit();
+                edit.putString(DATE_SP_KEY, curDateStr);
+                edit.commit();
+                Log.i("run","更新日期结束：" + curDateStr);
 
 
-
-            }
             /*for(Element td : tds){
                 Log.i(TAG,"run:td="+td);
                 Log.i(TAG, "run: text="+td.text());
                 Log.i(TAG, "run: html="+td.html());
             }*/
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+
 
 
         Message msg = handler.obtainMessage(7);
